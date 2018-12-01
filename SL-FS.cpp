78634c0fbd3285b7d5e90ef9be1fd45db6a7a6c9
Brian Bastias <brianbastias@hotmail.com>
2018-12-01 01:42:30 -0300

    Version 0.2

--- SL-FS.cpp
+++ SL-FS.cpp
@@ -1,4 +1,5 @@
 #include <sys/stat.h>
+#include <string>
 #include <fstream>
 #include <iostream>
 using namespace std;
@@ -9,14 +10,17 @@ int main()
    string aux,folder_route_string;
    int option;
    while(1){
-      cout << "1.Add Folder: " << endl;
-      cout << "2.Exit" << endl;
+      cout << "1.Add Folder: \n";
+      cout << "2.Exit\n\n";
+      cout << "Enter an option: ";
+      cin >> option;
+      
       if(option == 1){
          folder_route_string = "./";
          cout << "Enter the name of the folder: ";
          cin >> aux;
          folder_route_string += aux;
-         const char *folder_route = str.c_str();
+         const char *folder_route = folder_route_string.c_str();
          mkdir(folder_route, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
       }else if(option == 2){
