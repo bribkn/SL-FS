185768141135f08df3cd222c4f8643cedc46bb2a
v1nland <martin.saavedra@mail.udp.cl>
2018-12-04 01:53:35 -0300

    Version 0.4

--- SL-FS.cpp
+++ SL-FS.cpp
@@ -1,117 +1,155 @@
-#include <sys/stat.h>
+#include <string>
+#include <vector>
 #include <string>
 #include <fstream>
-#include <vector>
 #include <iostream>
 #include <unistd.h>
-#include <string>
+#include <sys/stat.h>
 using namespace std;
 
 int counter_id = 0;
 
-class Folder{
+class File{
 public:
-   int id;
-   int size;
-   string name;
-   Folder *father = NULL;
-   vector <Folder *> sons;
-   vector <string> files;
+    int id;
+    int size;
+    string name;
 
-   Folder(string name){
-      this->id = counter_id;
-      this->name = name;
-      this->size = 0;
-      ++counter_id;
-   }
-   void setFather(Folder *f){
-      father = f;
-   }
-   void addSubFolder(Folder *f){
-      sons.push_back(f);
-   }
-   void addSize(Folder *f, int s){
-      f->size += s;
-      if(f->father !=NULL){
-         addSize(f->father,s);
-      }
-   }
-   string CurrentPath(Folder *f){
-      if(f->father != NULL){
-         return CurrentPath(f->father) + "/" + name;
-      }else{
-         return "root";
-      }
-   }
-   void addFile(string f){
-      files.push_back(f);
-   }
-   void printStatus(){
-      cout << "Name\tID\tSize\n";
-      cout << name << "\t" << id << "\t" << size << "\n";
-   }
-   void printCurrentFolder(){
-      for(int i = 0; i < sons.size(); ++i){
-         cout << sons[i]->name << "    ";
-      }
-      for(int i = 0; i < files.size(); ++i){
-         cout << files[i] << "    ";
-      }
-      cout << endl;
-   }
-   Folder *changeFolder(string n){
-      for(int i = 0; i < sons.size(); ++i){
-         if(sons[i]->name == n){
-            return sons[i];
-         }
-      }
-      return NULL;
-   }
+    File(string name){
+        this->id = counter_id;
+        this->name = name;
+        this->size = name.length();
+
+        ++counter_id;
+    }
 };
 
-int main()
-{
+class Folder{
+public:
+    int id;
+    int size;
+    string name;
 
-   string command;
-   string folder_name;
-   string file_name;
-   Folder *Root = new Folder("root");
-   Folder *CurrentFolder = Root;
+    Folder *father;
 
-   while(1){
-      cin >> command;
-      // cout << "Command in progress: " << command << endl;
-      if(command == "exit"){
-         break;
+    vector <Folder *> childFolders;
+    vector <File *> files;
 
-      }else if(command == "mkdir"){
-         cin >> folder_name;
-         Folder *aux = new Folder(folder_name);
-         aux->setFather(CurrentFolder);
-         CurrentFolder->addSubFolder(aux);
+    Folder( string name ){
+        this->id = counter_id;
+        this->name = name;
+        this->size = 0;
+        this->father = NULL;
 
-      }else if(command == "ls"){
-         CurrentFolder->printCurrentFolder();
+        ++counter_id;
+    }
 
-      }else if(command == "cd"){
-         cin >> folder_name;
-         if(folder_name == "root"){
-            CurrentFolder = Root;
-         }else if(folder_name == ".."){
-            CurrentFolder = CurrentFolder->father;
-         }else{
-            CurrentFolder = CurrentFolder->changeFolder(folder_name);
-         }
+    void setFather( Folder *f ){ father = f; }
 
-      }else if(command == "create"){
-         cin >> file_name;
-         CurrentFolder->addFile(file_name);
-         CurrentFolder->addSize(CurrentFolder,file_name.size());
-      }else if(command == "status"){
-         CurrentFolder->printStatus();
-      }else if(command == "pwd"){
-         cout << CurrentFolder->CurrentPath(CurrentFolder) << endl;
-      }
-   }
-   return 0;
+    string getCurrentPath( Folder *f ){
+        if(f->father != NULL)
+            return getCurrentPath(f->father) + "/" + name;
+        else
+            return "root";
+    }
+
+    void addSubFolder( Folder *f ){ childFolders.push_back(f); }
+    void addFile( File *f ){ files.push_back( f ); }
+
+    void updateSize( Folder *f, int s ){
+        f->size += s;
+
+        if(f->father != NULL)
+            updateSize(f->father,s);
+    }
+
+    void printStatus(){
+        cout << "Name\ti-node\tSize\n";
+
+        cout << name << "\t" << id << "\t" << size << "\n";
+    }
+
+    void printChilds(){
+        for(int i = 0; i < childFolders.size(); ++i)
+            cout << childFolders[i]->name << "    ";
+
+        for(int i = 0; i < files.size(); ++i)
+            cout << files[i]->name << "    ";
+
+        if ( !childFolders.size() && !files.size() ) {
+            cout << "[LS] No folders or files were found";
+        }
+
+        cout << endl;
+    }
+
+    Folder *changeFolder( string n ){
+        for(int i = 0; i < childFolders.size(); ++i)
+            if( childFolders[i]->name == n ){
+                cout << "[CD] Folder swapped to: " << childFolders[i]->getCurrentPath(childFolders[i]) << "\n";
+                return childFolders[i];
+            }
+
+        cout << "[CD] Folder not found\n";
+        return this;
+    }
+};
+
+int main(){
+    string command;
+    string file_name;
+    string folder_name;
+
+    Folder *root = new Folder("root");
+    Folder *CurrentFolder = root;
+
+    cout << "Commands list:\n";
+    cout << "\tmkdir foldername: makes a new folder\n";
+    cout << "\tls: shows current folder items\n";
+    cout << "\tcd foldername: navigates to foldername\n";
+    cout << "\tcreate filename.extension: makes a new file\n";
+    cout << "\tstatus: shows current status\n";
+    cout << "\tpwd: shows the absolute route of the current folder\n\n\n";
+
+    while(1){
+        // cout << "Command in progress: " << command << endl;
+        cout << "-> " << CurrentFolder->getCurrentPath( CurrentFolder ) << "$ ";
+        cin >> command;
+
+        if(command == "exit"){
+            break;
+        }else if(command == "mkdir"){
+            cin >> folder_name;
+            cout << "Creating new folder with name: " << folder_name << "\n\n";
+
+            Folder *aux = new Folder(folder_name);
+
+            aux->setFather(CurrentFolder);
+            CurrentFolder->addSubFolder(aux);
+        }else if(command == "ls"){
+            CurrentFolder->printChilds();
+        }else if(command == "cd"){
+            cin >> folder_name;
+
+            if(folder_name == "root" || folder_name == ""){
+                CurrentFolder = root;
+            }else if(folder_name == ".."){
+                CurrentFolder = CurrentFolder->father;
+            }else{
+                CurrentFolder = CurrentFolder->changeFolder(folder_name);
+            }
+        }else if(command == "create"){
+            cin >> file_name;
+            File *newFile = new File(file_name);
+
+            CurrentFolder->addFile( newFile );
+            CurrentFolder->updateSize( CurrentFolder, newFile->size );
+        }else if(command == "status"){
+            CurrentFolder->printStatus();
+        }else if(command == "pwd"){
+            cout << CurrentFolder->getCurrentPath(CurrentFolder) << endl;
+        }
+    }
+
+    return 0;
 }
